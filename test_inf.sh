9c8aaf908a0a98eff2349a969558576ef2991bda
Michael E. Elgin <melgin@uwyo.edu>
2025-09-07 15:57:24 -0600

    test_inf.sh and test commit

---
+++ test_inf.sh
@@ -0,0 +1,9 @@
+python llava/eval/run_llava_3d.py \
+    --model-path ChaimZhu/LLaVA-3D-7B \
+    --image-file https://llava-vl.github.io/static/images/view.jpg \
+    --query "What are the things I should be cautious about when I visit here?"
+
+python llava/eval/run_llava_3d.py \
+    --model-path ChaimZhu/LLaVA-3D-7B \
+    --image-file https://llava-vl.github.io/static/images/view.jpg \
+    --query "What is this?"
