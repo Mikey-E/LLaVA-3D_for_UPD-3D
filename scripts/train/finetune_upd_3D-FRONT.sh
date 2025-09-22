8a7f6c46a45d5c7e48311a3e6a3edfbcfd9d2c69
Michael E. Elgin <melgin@uwyo.edu>
2025-09-22 14:30:35 -0600

    finetuning scripts for 3D-FRONT and Crops3D_gpt-5-nano

---
+++ scripts/train/finetune_upd_3D-FRONT.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Uncomment and set the following variables correspondingly to run this script:
+JSON_FOLDER="./playground/data/annotations"
+
+#@@@orig
+# VIDEO_FOLDER="./playground/data/LLaVA-3D-Pretrain"
+#@@@new
+VIDEO_FOLDER="/project/3dllms/melgin/datasets/3d-grand_unzipped_gpt-5-nano/3D-FRONT"
+
+# --model_name_or_path ./checkpoints/llava-3d-7b-pretrain \
+# --data_path ${JSON_FOLDER}/llava-3d-pretrain.json \
+deepspeed llava/train/train_mem.py \
+    --deepspeed ./scripts/zero2.json \
+    --model_name_or_path ./checkpoints/LLaVA-3D-7B \
+    --version v1 \
+    --data_path ${JSON_FOLDER}/LLaVA-3D-Data/overall_3D-FRONT_train.json \
+    --vision_tower openai/clip-vit-large-patch14-336 \
+    --video_folder ${VIDEO_FOLDER} \
+    --video_tower SpatialAwareModule \
+    --num_frames 20 \
+    --num_sample_tokens 1152 \
+    --mm_projector_type mlp2x_gelu \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --bf16 True \
+    --output_dir ./checkpoints/LLaVA-3D-7B_ft-upd_3D-FRONT \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 2 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 8 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 24000 \
+    --save_total_limit 1 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 True \
+    --model_max_length 4096 \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 4 \
+    --lazy_preprocess True \
+    --report_to none
---
+++ slurm_finetune_upd_3D-FRONT.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#SBATCH --account=3dllms
+#SBATCH --partition=mb-l40s
+#SBATCH --job-name=llava-3d_ft_upd_3D-FRONT
+#SBATCH --output=./slurm_logs/llava-3d_ft_upd_3D-FRONT%j.out
+#SBATCH --error=./slurm_logs/llava-3d_ft_upd_3D-FRONT%j.out
+#SBATCH --gres=gpu:5
+#SBATCH --mem=144G
+#SBATCH --time=7-00:00:00
+
+#This ensures "conda activate <env>" works in non-interactive shells.
+#(running "conda init" every time won't work.)
+if [ -n "$CONDA_INSTALL_PATH" ]; then
+    CONDA_SH=$CONDA_INSTALL_PATH/etc/profile.d/conda.sh
+    if [ ! -e "$CONDA_SH" ]; then
+        echo "ERROR: $CONDA_SH does not exist."
+        exit 1
+    fi
+    source "$CONDA_SH"
+else
+    CONDA_SH=/project/3dllms/melgin/conda/etc/profile.d/conda.sh
+    echo "WARNING: CONDA_INSTALL_PATH is not set. Trying $CONDA_SH"
+    if [ ! -e "$CONDA_SH" ]; then
+        echo "ERROR: $CONDA_SH does not exist."
+        exit 1
+    fi
+    source "$CONDA_SH"
+fi
+# Now the activation should work
+conda activate llava-3d
+
+export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:21
+bash scripts/train/finetune_upd_3D-FRONT.sh
